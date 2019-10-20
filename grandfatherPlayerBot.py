3fdc74718917a480f54d082a3689c5921bcf5a88
Tim <botballmaster@hotmail.com>
2019-10-19 23:08:17 -0400

    Net grandfather to sys.arg[1]
    If you give the program a system argument of file that exists, it will attempt to use it  as the grandfather bot structure.

--- grandfatherPlayerBot.py
+++ grandfatherPlayerBot.py
@@ -1,6 +1,8 @@
 from playerBot import playerBot
 from nuralNetLayer import nuralNetLayer
 import json
+import sys
+import os
 
 class grandfatherPlayerBot(playerBot):
 
@@ -11,7 +13,12 @@ class grandfatherPlayerBot(playerBot):
         self.eg_nural_net = nuralNetLayer() # end game neural net
 
         # get the pre defined nural net for the hit / hold decision
-        with open('archive/grandfather.json') as nf:
+        if len(sys.argv) > 1 and os.path.exists(sys.argv[1]):
+            file = sys.argv[1]
+        else:
+            file = 'archive/grandfather.json'
+
+        with open(file) as nf:
             net = json.loads(nf.read())
 
             self.build_from_dict(net)
